24f8a04684fd1d0a60aca7d41324513c35f4b607
carcinos93 <nelson.rodas1993@gmail.com>
2019-05-20 17:35:41 -0600

    initial

---
+++ ReportesCLIN2/ReportesCLIN2/App_Start/DbFactory.cs
@@ -0,0 +1,29 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ReportesCLIN2.App_Start
+{
+    public class DbFactory
+    {
+        private static ILog log = LogManager.GetLogger(typeof(DbFactory));
+        public static IDbConnection Conn()
+        {
+            
+            var connectionString = @"server=clindev17;database={0};uid={1};password={2};";
+            HttpContext context = HttpContext.Current;
+            var usuario = "sa";
+            var empresa = "CLIN";
+            var password = "P@ssw0rd";
+           
+            var connection = new SqlConnection(
+                string.Format(connectionString, empresa, usuario, password)
+                );
+            return connection;
+        }
+    }
+}
